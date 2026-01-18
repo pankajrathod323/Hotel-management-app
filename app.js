e931171d58701309cf9af12b0966cd05fca88762
pankaj rathod <pankajrathod272004@gmail.com>
2026-01-18 09:53:04 +0530

    added show listings route /listings that display all listings

--- app.js
+++ app.js
@@ -20,23 +20,31 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-app.get("/testListing",async (req, res) =>{
-     let sampleListing = new Listing({
-      title: "My New Villa",
-      description: "By the beach",
-      price: 1200,
-      location: "Calangute, Goa",
-      country: "India",
-     })
-      await sampleListing.save();
-      console.log("sample was saved");
-     res.send("successful testing");
-})
+// app.get("/testListing",async (req, res) =>{
+//      let sampleListing = new Listing({
+//       title: "My New Villa",
+//       description: "By the beach",
+//       price: 1200,
+//       location: "Calangute, Goa",
+//       country: "India",
+//      })
+//       await sampleListing.save();
+//       console.log("sample was saved");
+//      res.send("successful testing");
+// })
 
 
 app.get("/", (req, res) => {
   res.send("hi i'am root");
-})
+});
+
+app.get("/listings", async (req, res) => {
+  const allListings =  await Listing.find({});
+  console.log(allListings);
+  res.render("listings/index.ejs", { allListings });
+});
+
+
 app.listen(port, () => {
   console.log("app is litening on port", port);
 })
