07d39d6e1f39c5285d51ac07bdfd6310a914d2c7
pankaj rathod <pankajrathod272004@gmail.com>
2026-01-20 18:47:58 +0530

    finshed styling of home new show index .ejs

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true})); 
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
+ 
 main().then(() => {
   console.log("successfully connected to database");
 }).catch((err) => {
