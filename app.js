152e16282ea1861b81eaad4626db5f808d90037c
pankaj rathod <pankajrathod272004@gmail.com>
2026-02-08 09:49:41 +0530

    add review Schema in review.js and review section in show.ejs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -114,6 +115,21 @@ app.delete(
     res.redirect("/listings");
   })
 );
+
+//Add Review
+// Review post
+
+app.post("/listings/:id/reviews", wrapAsync(async (req, res) => {
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  res.redirect(`/listings/${listing._id}`);
+}));
 
 // Show Route
 app.get(
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
   title: {
@@ -21,6 +22,12 @@ const listingSchema = new Schema({
   },
   location: String,
   country: String,
+  reviews: [
+    { 
+      type: Schema.Types.ObjectId,
+      ref: Review,
+    }
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
